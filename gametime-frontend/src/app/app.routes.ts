c66b144231dd3841064b32331ed226a243312765
Joao Santana <joao.ssantana@dataprev.gov.br>
2025-07-12 01:52:15 +0000

    refactor: corrigir nome de arquivos

--- gametime-frontend/src/app/app.routes.ts
+++ gametime-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { GameListComponent } from './pages/game-list/game-list';
+// O caminho agora está correto e importa a classe com o nome corrigido
+import { CadastraGameComponent } from './pages/cadastra-game/cadastra-game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: GameListComponent },
+  { path: 'cadastrar', component: CadastraGameComponent }
+];
---
+++ gametime-frontend/src/app/pages/cadastra-game/cadastra-game.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastra-game',
+  standalone: true, // Adicionado para ser um componente standalone
+  imports: [],
+  templateUrl: './cadastra-game.html',
+  styleUrl: './cadastra-game.scss'
+})
+// O nome da classe foi alterado para corresponder ao que as rotas esperam
+export class CadastraGameComponent {
+
+}
